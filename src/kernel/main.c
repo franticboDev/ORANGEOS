876fc76ad6d708730b6e6e1c6a233b640913db65
franticboDev <it.franticbo@gmail.com>
2023-11-23 12:23:40 +0200

    Added some things

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -37,9 +37,9 @@ void start(BootParams* bootParams)
     log_warn("Main", "This is a warning msg!");
     log_err("Main", "This is an error msg!");
     log_crit("Main", "This is a critical msg!");
-    printf("Nanobyte OS v0.1\n");
+    printf("Orange OS v0.1\n");
     printf("This operating system is under construction.\n");
-    //i686_IRQ_RegisterHandler(0, timer);
+    i686_IRQ_RegisterHandler(0, timer);
 
     //crash_me();
 
